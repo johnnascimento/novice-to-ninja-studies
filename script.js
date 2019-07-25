1680713a4a125008350d00ac82cdedbadd8cb5b1
johnnascimento <jlenonns@gmail.com>
2019-07-25 09:21:58 -0300

    changing approach and with that somw functionalities

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ window.onload = () => {
   let $inputEntry = dGetById('answerQuestion');
   let $btnChecker = dGetById('btnChecker');
   let $startBtn = dGetById('startBtn');
+  let $form = dGetById('answer');
   
   const quiz = {
     name: "Super hero Quiz",
@@ -27,6 +28,8 @@ window.onload = () => {
       { name: "Batman",realName: "Bruce Wayne" }
     ]
   };
+  
+  let max = (quiz.questions.length)-1;
   
   // View functions
   const update = (elem, content, klass) => {
@@ -40,44 +43,64 @@ window.onload = () => {
         p.className = klass;
       }
   };
-   
-   
-  let max = (quiz.questions.length)-1;
+  
+  const showElement = (elem) => {
+    elem.style.display = "block";
+  };
+  
+  const hideElement = (elem) => {
+    elem.style.display = "none";
+  };
+  
+  hideElement($form);
   
   const playQuiz = (quiz) => {
-    score = 0;
-    update($score, score);
-    //update($question, question);
+    hideElement($startBtn);
+    console.log('start btn hidden');
+    showElement($form);
+    console.log('show form activated');
     
-    for(var i=0; i<=max; i++) {
-      // Main game loop
-      let question = `What\'s ${quiz.questions[i].name}\'s real name?`;
-      let answer = ask(question);
-      check(answer, i);
-    }
-    // End of main loop
-    gameOver();
+    score = 0;
+    let question;
+    let i = 0;
+    update($score, score);
+    console.log('playquiz update function executed');
+    
+    $form.addEventListener('submit', (ev) => {
+      ev.preventDefault();
+      check($form[0].value, i);
+    }, false);
+    
+    chooseQuestion(i);
+    console.log('choose question executed');
   };
+  
+  
+  const chooseQuestion = (i) => {
+    console.log('It came into the choose question');
+    question = `What\'s ${quiz.questions[i].name}\'s real name?`;
+    ask(question);
+    
+    i++;
+    console.log(i);
+    if(i === quiz.questions.length) {
+      console.log('came into IF of choose question');
+      gameOver();
+    } else {
+      chooseQuestion(i);
+    }
+  };
+  
   
   const ask = (question) => {
     update($question, question);
-    let counter = 20;
-   /* var myInterval = setInterval(() => {
-      $counter.innerHTML = counter;
-      counter--;
-      
-      if(counter < 0) {
-        gameOver();
-        clearInterval(myInterval);
-      }
-    }, 1000);*/
-    return prompt(question);
-    
+    $form[0].value = "";
+    $form.focus();
   };
   
   const check = (answer, idx) => {
     // Check if the answer is correct
-    if(answer.toLowerCase() === quiz.questions[idx].realName.toLowerCase()) {
+    if(answer.toLowerCase() === quiz.questions[i].realName.toLowerCase()) {
       update($feedback, 'correct', 'right');
       //Increase score by 1
       score++;
@@ -88,7 +111,8 @@ window.onload = () => {
   };
   
   const gameOver = () => {
-    // Inform the player that the game has finished and tell them how many points they've made
+    hideElement($form);
+    showElement($startBtn);
     console.log('gameOver() invoked');
     update($question, `Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`, 'gameOver');
   };
