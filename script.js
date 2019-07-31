04da51d1cbce6158bf4388f68748a4b010ac3838
John Lenon Nascimento da Silva <jnascimento@itelios.com>
2019-07-31 09:36:16 -0300

    Script js changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = () => {
   const dQuery = elem => { return d.querySelector(elem) };
   const dGetById = elem => { return d.getElementById(elem) };
   const dGetByClass = elem => { return d.getElementsByClassName(elem) };
-  
+
   ///// score initialization and DOM references
   let score;
   let $question = dGetById('question');
@@ -18,7 +18,7 @@ window.onload = () => {
   let $btnChecker = dGetById('btnChecker');
   let $startBtn = dGetById('startBtn');
   let $form = dGetById('answer');
-  
+
   const quiz = {
     name: "Super hero Quiz",
     description: "Hiw many super heroes can you name?",
@@ -28,14 +28,14 @@ window.onload = () => {
       { name: "Batman",realName: "Bruce Wayne" }
     ]
   };
-  
+
   let max = (quiz.questions.length)-1;
-  
+
   // View functions
   const update = (elem, content, klass) => {
     console.log(elem);
     var p;
-    
+
       p = (!elem.firstChild) ? d.createElement('p') : elem.firstChild;
       p.textContent = content;
       elem.appendChild(p);
@@ -43,64 +43,69 @@ window.onload = () => {
         p.className = klass;
       }
   };
-  
+
   const showElement = (elem) => {
     elem.style.display = "block";
   };
-  
+
   const hideElement = (elem) => {
     elem.style.display = "none";
   };
-  
+
   hideElement($form);
-  
+
   const playQuiz = (quiz) => {
     hideElement($startBtn);
     console.log('start btn hidden');
     showElement($form);
     console.log('show form activated');
-    
+
     score = 0;
     let question;
     let i = 0;
     update($score, score);
     console.log('playquiz update function executed');
-    
+
     $form.addEventListener('submit', (ev) => {
       ev.preventDefault();
       check($form[0].value, i);
+      i++;
     }, false);
-    
+
     chooseQuestion(i);
     console.log('choose question executed');
   };
-  
-  
+
+
   const chooseQuestion = (i) => {
     console.log('It came into the choose question');
     question = `What\'s ${quiz.questions[i].name}\'s real name?`;
+    console.log('Question: ', question);
     ask(question);
-    
-    i++;
-    console.log(i);
-    if(i === quiz.questions.length) {
-      console.log('came into IF of choose question');
-      gameOver();
-    } else {
-      chooseQuestion(i);
-    }
   };
-  
-  
+
+
   const ask = (question) => {
+    console.log('It came into the ASK function');
     update($question, question);
     $form[0].value = "";
     $form.focus();
+    console.log('It came OUT the ASK function');
   };
-  
+
   const check = (answer, idx) => {
     // Check if the answer is correct
-    if(answer.toLowerCase() === quiz.questions[i].realName.toLowerCase()) {
+    console.log('++++++++++++++',quiz.questions[idx]);
+
+    console.log(idx);
+    if(idx === quiz.questions.length) {
+      console.log('came into IF of choose question');
+      gameOver();
+    } else {
+      chooseQuestion(idx);
+    }
+
+    if(answer.toLowerCase() === quiz.questions[idx].realName.toLowerCase()) {
       update($feedback, 'correct', 'right');
       //Increase score by 1
       score++;
@@ -109,14 +114,14 @@ window.onload = () => {
       update($feedback, 'wrong', 'wrong');
     }
   };
-  
+
   const gameOver = () => {
     hideElement($form);
     showElement($startBtn);
     console.log('gameOver() invoked');
     update($question, `Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`, 'gameOver');
   };
-  
+
   $startBtn.addEventListener('click', () => {
       return playQuiz(quiz);
     });
